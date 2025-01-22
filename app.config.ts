3739ef311a83deb1da3697642bac5d0f01749bf8
Ravikumar Padmanaban <ravikumar@aveoninfotech.com>
2025-01-22 21:55:43 +0530

    add button and input with styles

---
+++ app.config.ts
@@ -0,0 +1,6 @@
+import './assets/css/core.css';
+import './assets/css/container.css';
+import 'primeicons/primeicons.css';
+import './assets/css/overrides.css';
+
+export default defineAppConfig({});
