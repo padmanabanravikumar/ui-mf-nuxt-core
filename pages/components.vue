a77f1c9fe3e3c7782ce123a6d1c7982ba913ad39
Ravikumar Padmanaban <ravikumar@aveoninfotech.com>
2025-01-23 09:49:10 +0530

    use app form

--- pages/components.vue
+++ pages/components.vue
@@ -16,7 +16,7 @@
         <h3 class="m-0">Create Service</h3>
       </template>
       <template #content>
-        <form class="inline-flex flex-column gap-16 mt-16">
+        <AppForm class="inline-flex flex-column gap-16 mt-16" :initialValues={initialValues}>
           <AppFormField label="Name" name="name" required>
             <AppInputText type="text" name="name" placeholder="Name" />
           </AppFormField>
@@ -64,7 +64,7 @@
             <Button rounded label="Reset" severity="secondary" />
             <Button rounded label="Delete" severity="danger" />
           </div>
-        </form>
+        </AppForm>
       </template>
     </Card>
     <div hidden>
